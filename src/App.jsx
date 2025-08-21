ab4dc7025e65adffec2fffa6ade1acb62e300546
Shruti Tokekar <184140612+ShrutiTokekar@users.noreply.github.com>
2025-08-20 23:36:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from './assets/logo.jpeg'
 //import 'animate.css';
 import React from "react";
 import './App.css'
